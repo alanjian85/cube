1f3ec983b4c20ba644f4cb491900ae85cec5d688
Alan Jian <alanjian85@outlook.com>
2022-01-12 22:03:21 +0800

    Getting a window on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
+#include <cstdlib>
 #include <iostream>
 
+#include <SDL2/SDL.h>
+#include <spdlog/spdlog.h>
+
+const int window_width = 640;
+const int window_height = 480;
+
 int main() {
-    std::cout << "Hello world!\n";
-    return 0;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        spdlog::error("SDL could not initialize! SDL Error: {}\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Cube", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        spdlog::error("Window could not be created! SDL Error: {}\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+                spdlog::info("SDL quitted");
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
